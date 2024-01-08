9a8b6ce94a7c734ba6012eff18af6b0bcb0946fb
Bilol Sanatillaev <sanatillayevbilol@gmail.com>
2024-01-08 15:03:13 +0500

    Initial Commit

---
+++ HotelBooking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotelBooking
+//
+//  Created by Bilol Sanatillayev on 08/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotelBooking/HotelBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingApp.swift
+//  HotelBooking
+//
+//  Created by Bilol Sanatillayev on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
